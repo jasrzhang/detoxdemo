d9d362f63c1f5ae0cd2a44676a3761f914e043b3
jasrzhang <zhangshurui1989@gmail.com>
2024-03-12 16:46:08 +1100

    updated test code to see if can avoid idling

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -8,16 +8,31 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
+    await waitFor(element(by.id('welcome')))
+      .toBeVisible()
+      .withTimeout(5000);
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
   it('should show hello screen after tap', async () => {
+    await waitFor(element(by.id('welcome')))
+      .toBeVisible()
+      .withTimeout(5000);
     await element(by.id('welcome')).scrollTo('bottom');
+    await waitFor(element(by.text('Help')))
+      .toBeVisible()
+      .withTimeout(5000);
     await expect(element(by.text('Help'))).toBeVisible();
   });
 
   it('should show world screen after tap', async () => {
+    await waitFor(element(by.id('welcome')))
+      .toBeVisible()
+      .withTimeout(5000);
     await element(by.id('welcome')).scrollTo('top');
+    await waitFor(element(by.text('Step One')))
+      .toBeVisible()
+      .withTimeout(5000);
     await expect(element(by.text('Step One'))).toBeVisible();
   });
 });
