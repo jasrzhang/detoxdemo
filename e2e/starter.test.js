95891c241dd36f42d80502e0ce79f10633a6e5d2
jasrzhang <zhangshurui1989@gmail.com>
2024-03-12 16:58:52 +1100

    clean test file only do simple tests try use cache for gradle to increase efficiency

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -11,28 +11,5 @@ describe('Example', () => {
     await waitFor(element(by.id('welcome')))
       .toBeVisible()
       .withTimeout(5000);
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await waitFor(element(by.id('welcome')))
-      .toBeVisible()
-      .withTimeout(5000);
-    await element(by.id('welcome')).scrollTo('bottom');
-    await waitFor(element(by.text('Help')))
-      .toBeVisible()
-      .withTimeout(5000);
-    await expect(element(by.text('Help'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await waitFor(element(by.id('welcome')))
-      .toBeVisible()
-      .withTimeout(5000);
-    await element(by.id('welcome')).scrollTo('top');
-    await waitFor(element(by.text('Step One')))
-      .toBeVisible()
-      .withTimeout(5000);
-    await expect(element(by.text('Step One'))).toBeVisible();
   });
 });
